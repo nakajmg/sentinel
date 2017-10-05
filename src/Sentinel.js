c88b3372b72f4f6ae07e33f6869708c8aae006cb
nakajmg <nakajima.jmg@gmail.com>
2017-10-06 07:49:06 +0900

    update methods

--- src/Sentinel.js
+++ src/Sentinel.js
@@ -3,6 +3,8 @@ import UAParser from 'ua-parser-js'
 import format from 'date-fns/format'
 import debounce from 'lodash-es/debounce'
 import filter from 'lodash-es/filter'
+import reduce from 'lodash-es/reduce'
+import assign from 'object-assign'
 
 class Sentinel {
   /**
@@ -41,7 +43,8 @@ class Sentinel {
       serverIP: this.serverIP,
       port: this.port,
       endPoint: this.endPoint,
-      observable: this.observable
+      observable: this.observable,
+      entryTypes: this.entryTypes,
     }
   }
   /**
@@ -49,7 +52,7 @@ class Sentinel {
    * @param {Object} parameters
    */
   set options(parameters) {
-    let {protocol, serverIP, port, endPoint, observable} = parameters
+    let {protocol, serverIP, port, endPoint, observable, entryTypes} = parameters
     /**
      * @private
      * @default 'http'
@@ -80,6 +83,12 @@ class Sentinel {
      * @type {boolean}
      */
     this.observable = observable ? observable : false
+    this.entryTypes = assign({}, {
+      resource: true,
+      mark: false,
+      measure: true,
+      paint: false,
+    }, entryTypes)
   }
 
   /**
@@ -105,25 +114,87 @@ class Sentinel {
    * @return {Promise<Object, Error>}
    * @desc Send performance.timing
    */
-  sendNavigationTiming() {
+  sendTiming() {
     return this._patch(this.itemURL, {
-      navigationTiming: window.performance.timing
+      timing: window.performance.timing
     })
   }
 
   /**
    * @public
    * @return {Promise<Object, Error>}
-   * @desc Send Resource Timing
-   * ToDo 3種類送信するメソッドに🐸
+   * @desc Send mark entries
    */
-  sendResourceTiming() {
-    const resource = window.performance.getEntriesByType('resource')
-    const resourceTiming = filter(resource, (item) => {
-      return item.name.indexOf(this.url) === -1
-    })
-    console.log(resourceTiming)
-    return this._patch(this.itemURL, {resourceTiming})
+  sendMark() {
+    const mark = window.performance.getEntriesByType('mark')
+    return this._patch(this.itemURL, {mark})
+  }
+  /**
+   * @public
+   * @return {Promise<Object, Error>}
+   * @desc Send measure entries
+   */
+  sendMeasure() {
+    const measure = window.performance.getEntriesByType('measure')
+    return this._patch(this.itemURL, {measure})
+  }
+  /**
+   * @public
+   * @return {Promise<Object, Error>}
+   * @desc Send paint entries
+   */
+  sendPaint() {
+    const paint = window.performance.getEntriesByType('paint')
+    return this._patch(this.itemURL, {paint})
+  }
+
+  /**
+   * @public
+   * @return {Promise<Object, Error>}
+   * @desc Send resource entries
+   */
+  sendResource() {
+    function reduceEntriesByName(entries) {
+      return filter(entries, (entry) => {
+        return entry.name.indexOf(this.url) === -1
+      })
+    }
+    const _resource = window.performance.getEntriesByType('resource')
+    const resource = reduceEntriesByName(_resource)
+    return this._patch(this.itemURL, {resource})
+  }
+
+  /**
+   * @public
+   * @return {Promise<Object, Error>}
+   * @desc Send PerformanceTiming
+   */
+  sendPerformance(options = {}) {
+    const {entryTypes} = options
+    const reduceEntriesByName = (entries) => {
+      return filter(entries, (entry) => {
+        return entry.name.indexOf(this.url) === -1
+      })
+    }
+    const entries = {
+      timing: window.performance.timing,
+    }
+    const params = reduce(entryTypes || this.options.entryTypes, (params, enabled, type) => {
+      if (enabled) {
+        switch (type) {
+          case 'resource':
+            params[type] = reduceEntriesByName(window.performance.getEntriesByType('resource'))
+            break
+          case 'mark':
+          case 'measure':
+          case 'paint':
+            params[type] = window.performance.getEntriesByType(type)
+            break
+        }
+      }
+      return params
+    }, entries)
+    return this._patch(this.itemURL, params)
   }
 
   /**
@@ -158,19 +229,11 @@ class Sentinel {
 
     const _report = debounce(async () => {
       this._stopObserve()
-      await this.sendResourceTiming()
+      await this.sendPerformance()
       this._startObserve()
     }, 1000)
-
     this.observer = this.observer || new window.PerformanceObserver(_report)
     this._startObserve()
-  }
-
-  /**
-   * @public
-   */
-  disablePerformanceObserver() {
-    this._stopObserve()
   }
 
   /**
@@ -178,9 +241,13 @@ class Sentinel {
    */
   _startObserve() {
     if (!this.observer) return
-    this.observer.observe({
-      entryTypes: ['resource', 'mark', 'measure']
-    })
+    const entryTypes = reduce(this.options.entryTypes, (ret, enabled, type) => {
+      if (enabled) {
+        ret.push(type)
+      }
+      return ret
+    }, [])
+    this.observer.observe({entryTypes})
   }
 
   /**
@@ -189,6 +256,13 @@ class Sentinel {
   _stopObserve() {
     if (!this.observer) return
     this.observer.disconnect()
+  }
+
+  /**
+   * @public
+   */
+  disablePerformanceObserver() {
+    this._stopObserve()
   }
 
   /**
