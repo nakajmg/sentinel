fa99c9718f315ca03f8d8cc16351e83c6eb44bef
nakajmg <nakajima.jmg@gmail.com>
2017-10-16 09:16:10 +0900

    add comment

--- src/RouterView.js
+++ src/RouterView.js
@@ -18,22 +18,31 @@ class RouterView extends Component {
     const Component = route.component
     const params = route.params
     return (
-      <div className="fadein" ref={(div) => {this.el = div}}>
+      <div className="fadein" ref={div => this.el = div}>
         <Component params={params} store={store}></Component>
       </div>
     )
   }
 
+  /**
+   * @desc transitionでふわっと表示されるようにする
+   */
   componentDidMount() {
     defer(() => {
       this.el.classList.add('fadein-enter')
     })
   }
 
+  /**
+   * @desc transitionでふわっと表示されるようにする
+   */
   componentWillUpdate() {
     this.el.classList.remove('fadein-enter')
   }
 
+  /**
+   * @desc transitionでふわっと表示されるようにする
+   */
   componentDidUpdate() {
     defer(() => {
       this.el.classList.add('fadein-enter')
