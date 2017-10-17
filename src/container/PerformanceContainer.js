d5c048ed8261e48f80e53f4baa31e3ba699d1632
nakajmg <nakajima.jmg@gmail.com>
2017-10-17 13:51:05 +0900

    update style

--- src/container/PerformanceContainer.js
+++ src/container/PerformanceContainer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {isUndefined} from 'lodash-es'
 import BrowserVersion from '../components/PerformanceList/BrowserVersion'
 import OSVersion from '../components/PerformanceList/OSVersion'
@@ -15,34 +15,58 @@ import './PerformanceContainer.css'
  * @example
  * <PerformanceContainer store={store} params={params} />
  */
-function PerformanceContainer({store, params}) {
-  const data = store.getPerformanceDataById(params)
-  if (isUndefined(data)) return <div>no match id</div>
-  const timing = formatNavigationTiming(data)
-  console.log(JSON.parse(JSON.stringify(data)))
-  const style = {
-    backgroundColor: '#434857',
-    padding: '0.5em',
+class PerformanceContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: 'navigation'
+    }
   }
-  return (
-    <div className="PerformanceContainer GridContainer">
-      <div className="PerformanceContainer-Env GridItem-Column2">
-        <BrowserVersion style={style} browser={data.env.browser} />
-        <OSVersion style={style} os={data.env.os} />
-        <Timestamp style={style} date={data.date} />
-      </div>
-      <div className="PerformanceContainer-NavigationTiming GridItem-Column1">
-        <div className="SwitchData">
-          <button className="SwitchData-Button">Navigation</button>
-          <button className="SwitchData-Button">Resource</button>
+  render() {
+    const store = this.props.store
+    const params = this.props.params
+
+    const data = store.getPerformanceDataById(params)
+    if (isUndefined(data)) return <div>no match id</div>
+    const timing = formatNavigationTiming(data)
+    const style = {
+      backgroundColor: '#434857',
+      padding: '0.5em',
+    }
+    return (
+      <div className="PerformanceContainer GridContainer">
+        <div className="PerformanceContainer-Env GridItem-Column2">
+          <BrowserVersion style={style} browser={data.env.browser} />
+          <OSVersion style={style} os={data.env.os} />
+          <Timestamp style={style} date={data.date} />
         </div>
-        <div className="SwitchData-Container">
-          <TimeLine timing={timing} title={"Navigation Timing"} />
-          <ResourceTimingList resource={data.resource} />
+        <div className="PerformanceContainer-NavigationTiming GridItem-Column1">
+          <div className="SwitchData">
+            <button
+              className={`SwitchData-Button ${this.state.active === 'navigation' ? '--Active' : ''}`}
+              onClick={() => this.setState({active: 'navigation'})}
+            >
+              Navigation
+            </button>
+            <button
+              className={`SwitchData-Button ${this.state.active === 'resource' ? '--Active' : ''}`}
+              onClick={() => this.setState({active: 'resource'})}
+            >
+              Resource
+            </button>
+          </div>
+          <div className="SwitchData-Container">
+            <div className={this.state.active !== 'navigation' ? '--Hide' : ''}>
+              <TimeLine timing={timing} title={"Navigation Timing"}/>
+            </div>
+            <div className={this.state.active !== 'resource' ? '--Hide' : ''}>
+              <ResourceTimingList resource={data.resource}/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default PerformanceContainer
